4a2620b4fcb6afb77647549fa9c868d4621c83db
YuHanLi777 <101082388+YuHanLi777@users.noreply.github.com>
2022-06-08 18:18:55 +0800

    update styles

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -16,37 +16,54 @@ type Props = {
 
 export const Timeline: FC<Props> = ({ leftData, rightData }) => {
     return <div className={classes.root}>
-        <VerticalTimeline
-            className={classes.timeline}
-            layout={'1-column-left'}
-            lineColor={'#0f0'}
-        >
-            <VerticalTimelineElement
-                icon={<SequoiaIcon />}
-                date={'2002 - 2006'}
-                iconClassName={classes.icon}
-                intersectionObserverProps={{
-                    rootMargin: '0px 0px 40px 0px'
-                }}
-            >
-                <p>
-                    Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-                </p>
-                <p>
-                    Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-                </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                icon={<SequoiaIcon />}
-                iconClassName={classes.icon}
-            >
-                <p>
-                    123123asddasdasdfasdf
-                </p>
-            </VerticalTimelineElement>
-        </VerticalTimeline>
         <div className={classes.agendaList}>
             <OppTable data={leftData} className={classes.agendaItem} />
+            <VerticalTimeline
+                className={classes.timeline}
+                lineColor={'transparent'}
+            >
+                <VerticalTimelineElement
+                    icon={<SequoiaIcon />}
+                    iconClassName={classes.icon}
+                    intersectionObserverProps={{
+                        rootMargin: '0px 0px 40px 0px'
+                    }}
+                >
+                    <p>
+                        阿巴阿巴，阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴
+                    </p>
+
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    icon={<SequoiaIcon />}
+                    style={{ marginTop: -70 }}
+                    iconClassName={classes.icon}
+                >
+                    <p>
+                        阿巴阿巴，阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    icon={<SequoiaIcon />}
+                    position={'right'}
+                    style={{ marginTop: 224 }}
+                    iconClassName={classes.icon}
+                >
+                    <p>
+                        阿巴阿巴，阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    icon={<SequoiaIcon />}
+                    position={'left'}
+                    style={{ marginTop: 230 }}
+                    iconClassName={classes.icon}
+                >
+                    <p>
+                        阿巴阿巴，阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴
+                    </p>
+                </VerticalTimelineElement>
+            </VerticalTimeline>
             <OppTable data={rightData} className={classes.agendaItem} />
         </div>
     </div>
