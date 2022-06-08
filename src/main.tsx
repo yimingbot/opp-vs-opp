733b097b577671283ccf6e1e0d203896dd5b2d2e
ymzhou <ymzhou@sequoiacap.com>
2022-06-08 19:26:49 +0800

    chore: add data from xiaojie

--- src/main.tsx
+++ src/main.tsx
@@ -5,41 +5,100 @@ import { HackBackground } from "./components/HackBackground/HackBackground";
 import { Timeline } from "./components/Timeline/Timeline";
 
 const dataSource = {
-  Finance: {
-    营收: 1,
-    毛利: 2,
-    净利: 3,
-    成本: 3,
-    开支: 3,
-  },
-  Risk: {
-    负债: 1,
-    诉讼量: 1,
-    股权风险: 1,
-    政策风险: 1,
-    舆情: 1,
+  Talent: {
+    创始人背景: '程维',
+    创始人履历: '高管',
+    股权分布: '32%',
+    创始人风险: '',
   },
   Market: {
-    市场份额: 3,
-    'DAU/MAU': 1,
-    获客成本: 3,
-    客单价: 3,
-    GMV: 3,
-    停留时长: 3,
-    付费率: 3,
-    市场规模: 3,
+    市场规模: '用户量1.8亿',
+    市场份额: '43.3%',
+    DAU: '500w',
+    MAU: '3000w',
+    GMV: '$1500M',
+    获客成本: '$1.8',
   },
-  Talent: {
-    创始人背景: 1,
-    创始人履历: 2,
-    股权分布: 2,
+  Operation: {
+    成立时间: '2012年',
+    核心业务: '快车，专车',
+    核心优势:	'智能调度，用户画像',
+    开通城市: '24座',
+    司机数量: '6万',
+    最新轮次: 'D轮',
+    总融资金额: '$800M',
+    主要融资方: '金沙江，腾讯，淡马锡',
+  },
+  Risk: {
+    负债: '200M',
+    诉讼量: 120,
+    舆情: '较正向',
+    利益关联方: '腾讯',
+  },
+  Finance: {
+    营收: '$12B',
+    毛利: '$100M',
+    净利: '-$5B',
+    开支: '$2B',
+  },
+  'Sequoia Position': {
+    进入轮次: 'B轮',
+    董事会席位: 1,
+    股权占比: '4%',
+    投资占比: '10%',
   }
 }
+
+const dataSource2 = {
+  Talent: {
+    创始人背景: '陈伟星',
+    创始人履历: '连续创业者',
+    股权分布: '25%',
+    创始人风险: '',
+  },
+  Market: {
+    市场规模: '用户量1.8亿',
+    市场份额: '56.5%',
+    DAU: '400w',
+    MAU: '23000w',
+    GMV: '$1200M',
+    获客成本: '$2.2',
+  },
+  Operation: {
+    成立时间: '2012年',
+    核心业务: '快车',
+    核心优势:	'',
+    开通城市: '28座',
+    司机数量: '10万',
+    最新轮次: 'D轮',
+    总融资金额: '$800M',
+    主要融资方: '阿里，软银，老虎',
+  },
+  Risk: {
+    负债: '320M',
+    诉讼量: 181,
+    舆情: '',
+    利益关联方: '',
+  },
+  Finance: {
+    营收: '$15B',
+    毛利: '$80M',
+    净利: '-$6B',
+    开支: '$350M',
+  },
+  'Sequoia Position': {
+    进入轮次: '',
+    董事会席位: '',
+    股权占比: '',
+    投资占比: '',
+  }
+}
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HackBackground>
-      <Timeline leftData={dataSource} rightData={dataSource} />
+      <Timeline leftData={dataSource} rightData={dataSource2} />
     </HackBackground>
   </React.StrictMode>
 )
